816eb731a5ba768997d32c7a673e8363eae1dc10
Serhii_Veselov <serhii.veselov@gmail.com>
2023-12-20 13:39:29 +0200

    Test commit

--- src/test/java/stepdefs/FiltersSteps.java
+++ src/test/java/stepdefs/FiltersSteps.java
@@ -13,7 +13,6 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.extension.Extension;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 
